e31c7b73ac45731a8be061658f9de08e0f451790
Taro <pxm06474@hotmail.co.jp>
2026-09-12 00:48:34 +0900

    Tagモデルとconfirm処理を追加

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,14 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Category;
+use App\Models\Tag;
+
+
 class ContactController extends Controller
 {
+
+
     public function index()
     {
 
-    return view('contact.index');
+    $categories = Category::all();
+
+    return view('contact.index', ['categories' => $categories]);
 
     }
+
+
+    public function confirm()
+    {
+
+    $tags = Tag::all();
+
+    return view('contact.confirm', ['tags' => $tags]);
+
+
+    }
+
 
 
 }
